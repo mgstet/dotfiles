bc69db3efeedc00c41f47127d13af2d6e4592416
mgstet <marco.gstettenhofer@gmx.at>
2024-02-23 15:42:42 +0100

    Updated Paths

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,6 +1,9 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+set PATH /usr/local/go/bin $PATH
+
 starship init fish | source
 
 alias icat='kitty +kitten icat'
